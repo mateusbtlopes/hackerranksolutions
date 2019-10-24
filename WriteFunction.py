87308a859c166dd0cd47136560a211a4121ea423
Mateus Bruno T. Lopes <mateus.redes@gmail.com>
2019-10-24 20:08:35 -0300

    Create WriteFunction.py
    Write a function

---
+++ WriteFunction.py
@@ -0,0 +1,10 @@
+def is_leap(year):
+    leap = False
+    if year % 4 != 0:
+        leap = False
+    elif year % 4 == 0:
+        if (year % 100 == 0) and (year % 400 != 0):
+            leap = False
+        else:
+            leap = True
+    return leap
