e21f25a2b1214edaac7a1f3b2c126d5ea69031fd
Mateus Bruno T. Lopes <mateus.redes@gmail.com>
2019-10-24 20:59:51 -0300

    Create symmetric_difference_Operation.py
    Set .symmetric_difference() Operation

---
+++ symmetric_difference_Operation.py
@@ -0,0 +1,9 @@
+def read_set():
+    n = int(input())
+    return set(input().split())
+
+def main():
+    print(len(read_set().symmetric_difference(read_set())))
+
+if __name__ == '__main__':
+    main()
