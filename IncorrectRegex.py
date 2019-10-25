9907143acc3af605a3664850e1dff360e83eeb2a
Mateus Bruno T. Lopes <mateus.redes@gmail.com>
2019-10-24 21:20:08 -0300

    Create IncorrectRegex.py
    Incorrect Regex

---
+++ IncorrectRegex.py
@@ -0,0 +1,20 @@
+import re
+
+
+def solve(S):
+    try:
+        re.compile(S)
+        return True
+    except re.error:
+        return False
+
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        S = input()
+        print(solve(S))
+
+
+if __name__ == '__main__':
+    main()
