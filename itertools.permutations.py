753062e11626d5509978ee99ea55a5b940ba556c
Mateus Bruno T. Lopes <mateus.redes@gmail.com>
2019-10-24 21:06:36 -0300

    Create itertools.permutations.py
    itertools.permutations()

---
+++ itertools.permutations.py
@@ -0,0 +1,10 @@
+import itertools
+
+def main():
+    S, k = input().split()
+    k = int(k)
+    
+    print(*map(lambda permutation: ''.join(permutation), itertools.permutations(sorted(S), k)), sep='\n')
+
+if __name__ == '__main__':
+    main()
