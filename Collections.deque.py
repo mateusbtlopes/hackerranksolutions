0bac9370251eeb461b9d6abf5de86de870c78b3a
Mateus Bruno T. Lopes <mateus.redes@gmail.com>
2019-10-24 21:15:37 -0300

    Create Collections.deque.py
    Collections.deque()

---
+++ Collections.deque.py
@@ -0,0 +1,14 @@
+import collections
+
+def main():
+    N = int(input())
+    
+    d = collections.deque()
+    for _ in range(N):
+        command, *args = input().split()
+        getattr(d, command)(*args)
+    
+    print(*d)
+
+if __name__ == '__main__':
+    main()
