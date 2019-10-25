8fff5fe8b4004b77163720de872aa039b675c6f4
Mateus Bruno T. Lopes <mateus.redes@gmail.com>
2019-10-24 21:04:20 -0300

    Create CheckStrictSuperset.py
    Check Strict Superset

---
+++ CheckStrictSuperset.py
@@ -0,0 +1,11 @@
+def read_set():
+    return set(input().split())
+
+def main():
+    A = read_set()
+    N = int(input())
+    sets = [read_set() for _ in range(N)]
+    print('True' if all(map(lambda s: A.union(s) == A and A != s, sets)) else 'False')
+
+if __name__ == '__main__':
+    main()
