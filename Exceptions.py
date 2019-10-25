b88ddeb582575b2aad2ba581cd90a7846dbe5e07
Mateus Bruno T. Lopes <mateus.redes@gmail.com>
2019-10-24 21:19:09 -0300

    Create Exceptions.py
    Exceptions

---
+++ Exceptions.py
@@ -0,0 +1,17 @@
+def solve(a, b):
+    return int(a) // int(b)
+
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        a, b = input().split()
+
+        try:
+            print(solve(a, b))
+        except Exception as e:
+            print('Error Code:', e)
+
+
+if __name__ == '__main__':
+    main()
