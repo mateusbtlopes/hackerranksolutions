d20fecc9a5036a162a6889a501c3a96d3e408e8b
Mateus Bruno T. Lopes <mateus.redes@gmail.com>
2019-10-24 21:11:40 -0300

    Create MaximizeIt.py
    Maximize It!

---
+++ MaximizeIt.py
@@ -0,0 +1,13 @@
+import itertools
+
+def read_numbers():
+    return map(int, input().split())
+
+def main():
+    K, M = read_numbers()
+    lists = [list(read_numbers())[1:] for _ in range(K)]
+    
+    print(max(map(lambda combination: sum(map(lambda x: x * x, combination)) % M, itertools.product(*lists))))
+
+if __name__ == '__main__':
+    main()
