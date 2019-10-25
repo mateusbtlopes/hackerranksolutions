c47720124992ba3898906732e6701c7951ab41d8
Mateus Bruno T. Lopes <mateus.redes@gmail.com>
2019-10-24 21:05:51 -0300

    Create itertools.product.py
    itertools.product()

---
+++ itertools.product.py
@@ -0,0 +1,13 @@
+import itertools
+
+def read_list():
+    return map(int, input().split())
+
+def main():
+    A = read_list()
+    B = read_list()
+    
+    print(*itertools.product(A, B))
+
+if __name__ == '__main__':
+    main()
