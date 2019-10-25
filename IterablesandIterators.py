5f66e6035e1c1d47dc9a1b31e6a0cf938aeccd65
Mateus Bruno T. Lopes <mateus.redes@gmail.com>
2019-10-24 21:10:54 -0300

    Create IterablesandIterators.py
    Iterables and Iterators

---
+++ IterablesandIterators.py
@@ -0,0 +1,13 @@
+import itertools
+
+def main():
+    N = int(input())
+    letters = input().split()
+    K = int(input())
+    
+    combinations = list(itertools.combinations(letters, K))
+    
+    print(len(list(filter(lambda combination: 'a' in combination, combinations))) / len(combinations))
+
+if __name__ == '__main__':
+    main()
